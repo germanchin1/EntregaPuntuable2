b28cbf51ff59a0d2a6c1d98492c6b8b1ca1d5a2a
Ger <germanromo2006@gmail.com>
2026-02-09 18:22:51 +0100

    crear repo

---
+++ puntuable2/src/components/TheExperience.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { TresObject } from '@tresjs/core'
+import { useLoop } from '@tresjs/core'
+import { shallowRef } from 'vue'
+import { OrbitControls } from '@tresjs/cientos'
+
+const { onBeforeRender } = useLoop()
+
+const boxRef = shallowRef<TresObject | null>(null)
+
+onBeforeRender(({ elapsed }) => {
+  if (boxRef.value) {
+    boxRef.value.rotation.y = elapsed
+    boxRef.value.rotation.z = elapsed
+  }
+})
+</script>
+
+<template>
+  <TresPerspectiveCamera make-default :position="[5, 5, 5]" :look-at="[0, 0, 0]" />
+  <OrbitControls :target="[0, 0, 0]" :enable-damping="true" />
+  <TresAmbientLight :intensity="0.5" color="white" />
+  <TresMesh ref="boxRef" :position="[0, 2, 0]">
+    <TresBoxGeometry :args="[1, 1, 1]" />
+    <TresMeshNormalMaterial />
+  </TresMesh>
+  <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" cast-shadow />
+  <TresAxesHelper />
+  <TresGridHelper :args="[10, 10]" />
+</template>
